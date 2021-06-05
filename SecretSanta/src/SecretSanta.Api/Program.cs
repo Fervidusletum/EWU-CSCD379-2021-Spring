1ae63438ccb5b34dd512e911f8434a7918eb9dd6
Geoff <fervidusletum@gmail.com>
2021-06-05 15:03:56 -0700

    Added Logger

--- SecretSanta/src/SecretSanta.Api/Program.cs
+++ SecretSanta/src/SecretSanta.Api/Program.cs
@@ -1,17 +1,44 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace SecretSanta.Api
 {
     public class Program
     {
+        public static ILoggerFactory LoggerFactory { get; set; }
+        public Microsoft.Extensions.Logging.ILogger Logger { get; }
+                = Program.LoggerFactory.CreateLogger<Program>();
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("[Api] Starting...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "[Api] Startup Failed.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
